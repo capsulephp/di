56a6bfc435a72a208464f94352ae6df3365c4f02
Paul M. Jones <pmjones@pmjones.io>
2025-03-20 18:16:10 -0500

    soothe PHP 8.4

--- src/Definitions.php
+++ src/Definitions.php
@@ -57,12 +57,12 @@ class Definitions extends stdClass
         return new Lazy\CallableNew($id);
     }
 
-    public function csEnv(string $varname, string $vartype = null) : Lazy\CsEnv
+    public function csEnv(string $varname, ?string $vartype = null) : Lazy\CsEnv
     {
         return new Lazy\CsEnv($varname, $vartype);
     }
 
-    public function env(string $varname, string $vartype = null) : Lazy\Env
+    public function env(string $varname, ?string $vartype = null) : Lazy\Env
     {
         return new Lazy\Env($varname, $vartype);
     }
